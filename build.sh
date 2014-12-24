5c1723d9cbec291aae3fc0a032798ec1052546ad
Mike Chevett <mchevett@gmail.com>
2014-12-23 20:36:36 -0500

    build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+failure(){
+	echo 'build failure' && exit 1;
+}
+
+cd $DIR/d3-timeline
+npm install;
+sudo npm link;
+
+cd $DIR/ethoinfo-framework
+npm install;
+sudo npm link;
+npm link d3-timeline;
+node bundler.js
+
+cd $DIR/ethoinfo-project-app
+npm install;
+sudo npm link;
+npm link ethoinfo-framework;
+node bundler.js
+
+cd $DIR/ethoinfo-framework/ionic
+
